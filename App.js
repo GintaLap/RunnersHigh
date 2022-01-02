19e7044a5fd4497b754f0103a990d41a27d4a311
Ginta <69529030+GintaLap@users.noreply.github.com>
2022-01-02 15:57:46 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,37 +1,58 @@
-import * as React from 'react';
-import {Text, View, StyleSheet, Image } from 'react-native';
+import React from 'react';
+import {Text, View, StyleSheet, Image, StatusBar } from 'react-native';
 import constants from 'expo-constants';
 
+
 export default function App() {
-    return(
-        <View style={styles.container}>
+  return (
+        <View style={style.container}>
+        <StatusBar style="auto" />
             <Text style={style.paragraph}>
                 Hello world from Group nG03
             </Text>
+            <Image style={style.photo} source={require('/photo.jpg')}/>
             <Text style={style.paragraph}>
-                Member: Ginta Lapinska
+                Member: <Text style={style.boldText}>Ginta Lapinska</Text>
             </Text>
             <Text style={style.paragraph}>
-                This is my Ginta Lapinska, first React Native application! Nice to see you here.
+                This is my Gintas Lapinskas, first React Native application! Nice to see you here.
             </Text>
-            <image style={styles.logo} source={require('.assets/photo.jpg')}/>
+            
         </View>
-    );
+  );
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
     container:{
         flex: 1,
         justifyContent: 'center',
-        paddingTop: Constants.statusBarHeight,
-        backgroundColor: '#ecf0f1',
-        padding: 8,
+        paddingTop: constants.statusBarHeight,
+        backgroundColor: '#f2f2f2',
+        padding: 10
+      ,
     },
     paragraph:{
-        margin:24,
-        fontSize:18,
-        fontWeight: 'regular',
+        margin:30,
+        padding:10,
+        fontSize:19,
+        fontWeight: 'normal',
         textAlign: 'center',
+        fontFamily: 'sans-serif-thin',
+        color:	 '#333333',
 
     },
+    photo:{
+    width: 150,
+    height: 150,
+    borderRadius: 150 / 2,
+    overflow: "hidden",
+    marginLeft:100
+    },
+
+    boldText:{
+      fontWeight: 'bold',
+      fontFamily: 'sans-serif-thin',
+      color:	 '#333333',
+
+    }
 });
