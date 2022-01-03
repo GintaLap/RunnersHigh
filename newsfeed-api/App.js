282d903f9e29d5914f14d230db3d24965ebd7a2a
Ginta <69529030+GintaLap@users.noreply.github.com>
2022-01-04 00:58:54 +0200

    Newsfeed-Api

---
+++ newsfeed-api/App.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View, Image } from 'react-native';
+
+const App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      'https://newsapi.org/v2/everything?q=tesla&from=2021-12-03&sortBy=publishedAt&apiKey=d84d24fb9afe4006b5e9c0730caf8cda'
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json.articles))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+
+  }, []);
+
+  return (
+    <View style={{ flex: 1, backgroundColor: '#ccc', padding: 10 }}>
+      <Text
+        style={{
+          fontSize: 18,
+          fontWeight: 'bold',
+          margin: 10,
+          padding: 14,
+          textAlign: 'center',
+        }}>
+        Home
+      </Text>
+      {isLoading ? (
+        <ActivityIndicator size={'large'} color={'#0000ff'} />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <View
+              style={{ marginTop: 5, backgroundColor: '#fff', padding: 14 }}>
+                  <Image style={{margin:20}} source={{uri:item.urlToImage}} />
+              <Text style={{ fontSize: 16, fontWeight: 'bold' }}>
+                {item.title}, {item.name}
+              </Text>
+              <Text>{item.description}</Text>
+              <Text>{item.content}</Text>
+            </View>
+          )}
+        />
+      )}
+    </View>
+  );
+};
+
+export default App;
